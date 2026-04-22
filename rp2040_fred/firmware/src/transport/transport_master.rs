1af1471cc3f9bda9b187564a6ed70e57737ec21b
Chris Andrews <chris@nodnol.org>
2026-04-22 19:06:36 +0100

    Add Bus

--- rp2040_fred/firmware/src/transport/transport_master.rs
+++ rp2040_fred/firmware/src/transport/transport_master.rs
@@ -1,40 +1,17 @@
-use core::hint::spin_loop;
 use core::ptr::addr_of_mut;
 
 use embassy_executor::Executor;
-use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Drive, Input, Level, Output, OutputOpenDrain, Pull};
-use embassy_rp::multicore::{spawn_core1, Stack};
-use embassy_rp::peripherals::{PIO0, PIO1};
-use embassy_rp::pio::{
-    Config, Direction, Instance, InterruptHandler, Pio, PioBatch, ShiftConfig, ShiftDirection, StateMachine
-};
-use embassy_rp::pio_programs::clock_divider::calculate_pio_clock_divider_value;
-use pio::SideSet;
-use rp_pac as pac;
-
+use embassy_rp::multicore::Stack;
 use embassy_time::{Duration, Timer};
 use heapless::spsc::{Consumer, Producer, Queue};
 use portable_atomic::{AtomicBool, AtomicU32, Ordering};
 use static_cell::StaticCell;
 
 use crate::resources::{Core1Resources, PioResources};
-use crate::transport::Transport;
-use rp2040_fred_protocol::bridge_proto::{MsgType, Packet, TRACE_SAMPLES_PER_PACKET};
 use rp2040_fred_protocol::trace_decode::{AxisSnapshot, FeedbackDecoder, FeedbackSnapshot};
 
-macro_rules! log_info {
-    ($($arg:tt)*) => {
-        defmt::info!($($arg)*);
-    };
-}
-
-bind_interrupts!(struct Pio0Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-});
-bind_interrupts!(struct Pio1Irqs {
-    PIO1_IRQ_0 => InterruptHandler<PIO1>;
-});
+mod bus;
+use bus::Bus;
 
 const FLAG_ENABLED: u8 = 1 << 0;
 
@@ -59,90 +36,11 @@ pub struct PioTransport {
     snapshot_valid: bool,
     telemetry_period_ms: u16,
     next_telemetry_due_ms: u64,
+    // bus: ThisBus<'a>,
 }
 
-impl PioTransport {
-    pub fn new(core1_resources: Core1Resources, pio_resources: PioResources) -> Self {
-        let trace_ring = TRACE_SAMPLE_RING.init(Queue::new());
-        let (producer, consumer) = trace_ring.split();
-
-        TRACE_CAPTURE_ENABLED.store(true, Ordering::Relaxed);
-        TRACE_QUEUE_DROP_COUNT.store(0, Ordering::Relaxed);
-        TRACE_RXSTALL_COUNT.store(0, Ordering::Relaxed);
-
-        // spawn_core1(
-        //     core1_resources.core1,
-        //     unsafe { &mut *addr_of_mut!(CORE1_STACK) },
-        //     move || core1_loop(pio_resources, producer),
-        // );
-
-        embassy_rp::multicore::spawn_core1(core1_resources.core1, unsafe { &mut *addr_of_mut!(CORE1_STACK) }, move || {
-            let executor1 = EXECUTOR1.init(Executor::new());
-            executor1.run(|spawner| spawner.spawn(core1_loop(pio_resources, producer).unwrap()))
-        });
-
-        Self {
-            trace_samples: consumer,
-            capture_enabled: false,
-            telemetry_enabled: false,
-            packet_seq: 1,
-            sample_seq: 0,
-            decoder: FeedbackDecoder::new(),
-            current_snapshot: FeedbackSnapshot {
-                sample_index: 0,
-                x: AxisSnapshot {
-                    negative: false,
-                    value: 0,
-                },
-                z: AxisSnapshot {
-                    negative: false,
-                    value: 0,
-                },
-                rpm_display: 0,
-                rpm_raw: 0,
-            },
-            snapshot_valid: false,
-            telemetry_period_ms: 100,
-            next_telemetry_due_ms: 0,
-        }
-    }
-
-    fn clear_trace_samples(&mut self) {
-        while self.trace_samples.dequeue().is_some() {}
-    }
-
-    fn reset_stream_state(&mut self) {
-        self.packet_seq = 1;
-        self.sample_seq = 0;
-        self.decoder = FeedbackDecoder::new();
-        self.current_snapshot = FeedbackSnapshot {
-            sample_index: 0,
-            x: AxisSnapshot {
-                negative: false,
-                value: 0,
-            },
-            z: AxisSnapshot {
-                negative: false,
-                value: 0,
-            },
-            rpm_display: 0,
-            rpm_raw: 0,
-        };
-        self.snapshot_valid = false;
-        self.next_telemetry_due_ms = 0;
-        TRACE_QUEUE_DROP_COUNT.store(0, Ordering::Relaxed);
-        TRACE_RXSTALL_COUNT.store(0, Ordering::Relaxed);
-        self.clear_trace_samples();
-    }
-
-    fn flags(&self) -> u8 {
-        if self.telemetry_enabled {
-            FLAG_ENABLED
-        } else {
-            0
-        }
-    }
-}
+use crate::transport::Transport;
+use rp2040_fred_protocol::bridge_proto::{MsgType, Packet, TRACE_SAMPLES_PER_PACKET};
 
 impl Transport for PioTransport {
     fn handle_request(&mut self, req: Packet, out: &mut [Packet; 2]) -> usize {
@@ -272,165 +170,90 @@ impl Transport for PioTransport {
     }
 }
 
+impl PioTransport {
+    pub fn new(core1_resources: Core1Resources, pio_resources: PioResources) -> Self {
+        let trace_ring = TRACE_SAMPLE_RING.init(Queue::new());
+        let (producer, consumer) = trace_ring.split();
+
+        // TRACE_CAPTURE_ENABLED.store(true, Ordering::Relaxed);
+        // TRACE_QUEUE_DROP_COUNT.store(0, Ordering::Relaxed);
+        // TRACE_RXSTALL_COUNT.store(0, Ordering::Relaxed);
+
+        embassy_rp::multicore::spawn_core1(
+            core1_resources.core1,
+            unsafe { &mut *addr_of_mut!(CORE1_STACK) },
+            move || {
+                let executor1 = EXECUTOR1.init(Executor::new());
+                executor1.run(|spawner| spawner.spawn(core1_loop(pio_resources, producer).unwrap()))
+            },
+        );
+
+        Self {
+            trace_samples: consumer,
+            capture_enabled: false,
+            telemetry_enabled: false,
+            packet_seq: 1,
+            sample_seq: 0,
+            decoder: FeedbackDecoder::new(),
+            current_snapshot: FeedbackSnapshot {
+                sample_index: 0,
+                x: AxisSnapshot {
+                    negative: false,
+                    value: 0,
+                },
+                z: AxisSnapshot {
+                    negative: false,
+                    value: 0,
+                },
+                rpm_display: 0,
+                rpm_raw: 0,
+            },
+            snapshot_valid: false,
+            telemetry_period_ms: 100,
+            next_telemetry_due_ms: 0,
+        }
+    }
+
+    fn clear_trace_samples(&mut self) {
+        while self.trace_samples.dequeue().is_some() {}
+    }
+
+    fn reset_stream_state(&mut self) {
+        self.packet_seq = 1;
+        self.sample_seq = 0;
+        self.decoder = FeedbackDecoder::new();
+        self.current_snapshot = FeedbackSnapshot {
+            sample_index: 0,
+            x: AxisSnapshot {
+                negative: false,
+                value: 0,
+            },
+            z: AxisSnapshot {
+                negative: false,
+                value: 0,
+            },
+            rpm_display: 0,
+            rpm_raw: 0,
+        };
+        self.snapshot_valid = false;
+        self.next_telemetry_due_ms = 0;
+        TRACE_QUEUE_DROP_COUNT.store(0, Ordering::Relaxed);
+        TRACE_RXSTALL_COUNT.store(0, Ordering::Relaxed);
+        self.clear_trace_samples();
+    }
+
+    fn flags(&self) -> u8 {
+        if self.telemetry_enabled {
+            FLAG_ENABLED
+        } else {
+            0
+        }
+    }
+}
+
 #[embassy_executor::task]
-async fn core1_loop(pio_resources: PioResources, mut trace_samples: Producer<'static, u32>) -> ! {
-    // address data-dir high for output.
-    let mut data_dir_a = Output::new(pio_resources.pin_26, Level::High);
-    data_dir_a.set_high();
-
-    let fred_bm_clock = pio::pio_file!(
-        "../pio/bus_master.pio",
-        select_program("fred_bm_clock"),
-        options(max_program_size = 32)
-    );
-
-    let fred_bm_control = pio::pio_file!(
-        "../pio/bus_master.pio",
-        select_program("fred_bm_control"),
-        options(max_program_size = 32)
-    );
-
-    let fred_bm_data_write = pio::pio_file!(
-        "../pio/bus_master.pio",
-        select_program("fred_bm_data_write"),
-        options(max_program_size = 32)
-    );
-
-    let fred_bm_data_read = pio::pio_file!(
-        "../pio/bus_master.pio",
-        select_program("fred_bm_data_read"),
-        options(max_program_size = 32)
-    );
-
-    let mut pio0 = Pio::new(pio_resources.pio0, Pio0Irqs);
-
-    let mut clock_sm = pio0.sm0;
-    let mut control_sm = pio0.sm1;
-    let mut write_sm = pio0.sm2;
-    let mut read_sm = pio0.sm3;
-
-    let clock_program = pio0.common.load_program(&fred_bm_clock.program);
-    let control_program = pio0.common.load_program(&fred_bm_control.program);
-    let write_program = pio0.common.load_program(&fred_bm_data_write.program);
-    let read_program = pio0.common.load_program(&fred_bm_data_read.program);
-
-    log_info!("PIO programs loaded");
-
-    let p0 = pio0.common.make_pio_pin(pio_resources.pin_0);
-    let p1 = pio0.common.make_pio_pin(pio_resources.pin_1);
-    let p2 = pio0.common.make_pio_pin(pio_resources.pin_2);
-    let p3 = pio0.common.make_pio_pin(pio_resources.pin_3);
-    let p4 = pio0.common.make_pio_pin(pio_resources.pin_4);
-    let p5 = pio0.common.make_pio_pin(pio_resources.pin_5);
-    let p6 = pio0.common.make_pio_pin(pio_resources.pin_6);
-    let p7 = pio0.common.make_pio_pin(pio_resources.pin_7);
-    let p8 = pio0.common.make_pio_pin(pio_resources.pin_8);
-    let p9 = pio0.common.make_pio_pin(pio_resources.pin_9);
-    let p10 = pio0.common.make_pio_pin(pio_resources.pin_10);
-    let p11 = pio0.common.make_pio_pin(pio_resources.pin_11);
-    let p12 = pio0.common.make_pio_pin(pio_resources.pin_12);
-    let p13 = pio0.common.make_pio_pin(pio_resources.pin_13);
-    let p14 = pio0.common.make_pio_pin(pio_resources.pin_14);
-    let p15 = pio0.common.make_pio_pin(pio_resources.pin_15);
-    let p16 = pio0.common.make_pio_pin(pio_resources.pin_16);
-    let p17 = pio0.common.make_pio_pin(pio_resources.pin_17);
-    let p18 = pio0.common.make_pio_pin(pio_resources.pin_18);
-    let p19 = pio0.common.make_pio_pin(pio_resources.pin_19);
-    let p20 = pio0.common.make_pio_pin(pio_resources.pin_20);
-    let p21 = pio0.common.make_pio_pin(pio_resources.pin_21);
-    let p22 = pio0.common.make_pio_pin(pio_resources.pin_22);
-    let p27 = pio0.common.make_pio_pin(pio_resources.pin_27);
-    // let p28 = pio0.common.make_pio_pin(pio_resources.pin_28);
-
-    let data_bus_pins = [&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7];
-
-    let addr_bus_pins = [&p8, &p9, &p10, &p11, &p12, &p13, &p14, &p15];
-
-    let control_pins = [
-        &p17, // RnW
-        &p18, // NMI
-        &p19, // IRQ
-        &p20, // FRED
-    ];
-
-    let clock_pins = [
-        &p16, // 1MHzE
-    ];
-
-    let data_dir_pin = [&p27];
-
-    clock_sm.set_pin_dirs(Direction::In, &data_bus_pins);
-    clock_sm.set_pin_dirs(Direction::Out, &addr_bus_pins);
-    clock_sm.set_pin_dirs(Direction::Out, &data_dir_pin);
-
-    // setting up control pins for open-drain output
-    clock_sm.set_pins(Level::Low, &control_pins);
-    clock_sm.set_pin_dirs(Direction::Out, &control_pins);
-    clock_sm.set_pins(Level::Low, &clock_pins);
-    clock_sm.set_pin_dirs(Direction::Out, &clock_pins);
-
-    clock_sm.clear_fifos();
-    control_sm.clear_fifos();
-    write_sm.clear_fifos();
-    read_sm.clear_fifos();
-
-    let mut clock_cfg = Config::default();
-    clock_cfg.use_program(&clock_program, &clock_pins);
-    clock_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 16_000_000);
-    clock_sm.set_config(&clock_cfg);
-
-    let mut control_cfg = Config::default();
-    control_cfg.use_program(&control_program, &control_pins);
-    control_cfg.set_out_pins(&addr_bus_pins);
-    control_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 40_000_000);
-    control_cfg.shift_out = ShiftConfig {
-        threshold: 32,
-        direction: ShiftDirection::Left,
-        auto_fill: false,
-    };
-    control_sm.set_config(&control_cfg);
-
-    let mut write_cfg = Config::default();
-    write_cfg.use_program(&write_program, &data_dir_pin);
-    write_cfg.set_out_pins(&data_bus_pins);
-    write_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 20_000_000);
-    write_cfg.shift_out = ShiftConfig {
-        threshold: 32,
-        direction: ShiftDirection::Left,
-        auto_fill: false,
-    };
-    write_sm.set_config(&write_cfg);
-
-    let mut read_cfg = Config::default();
-    read_cfg.use_program(&read_program, &[]);
-    read_cfg.set_in_pins(&data_bus_pins);
-    read_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 50_000_000);
-    read_cfg.shift_out = ShiftConfig {
-        threshold: 32,
-        direction: ShiftDirection::Left,
-        auto_fill: false,
-    };
-    read_sm.set_config(&read_cfg);
-
-    let mut batch = PioBatch::new();
-    batch.restart(&mut clock_sm);
-    batch.set_enable(&mut clock_sm, true);
-    batch.restart(&mut control_sm);
-    batch.set_enable(&mut control_sm, true);
-    batch.restart(&mut write_sm);
-    batch.set_enable(&mut write_sm, true);
-    batch.restart(&mut read_sm);
-    batch.set_enable(&mut read_sm, true);
-    batch.execute();
-
-    [16, 17, 18, 19, 20].iter().for_each(|n| {
-        pac::PADS_BANK0.gpio(*n).modify(|w| {
-            w.set_pue(true);  // pull-up enable
-            w.set_pde(false); // pull-down disable
-        });
-    });
-    
-    log_info!("PIO initialised on core1");
+async fn core1_loop(pio_resources: PioResources, mut _trace_samples: Producer<'static, u32>) -> ! {
+    let mut bus = Bus::setup(pio_resources);
 
     loop {
         // write/read loop:
@@ -442,39 +265,19 @@ async fn core1_loop(pio_resources: PioResources, mut trace_samples: Producer<'st
         // 4. Read one response byte from `F1`.
 
         for _ in 1..10 {
-            read_cycle(&mut control_sm, &mut read_sm, 0xFF).await;
+            bus.read_cycle(0xFF).await;
         }
-
-        write_cycle(&mut control_sm, &mut write_sm, 0xFF, 0xFF).await;
 
         for _ in 1..10 {
-            read_cycle(&mut control_sm, &mut read_sm, 0xFF).await;
+            bus.write_cycle(0xFF, 0xFF).await;
         }
 
-        read_cycle(&mut control_sm, &mut read_sm, 0xFF).await;
+        for _ in 1..10 {
+            bus.read_cycle(0xFF).await;
+        }
+
+        bus.read_cycle(0xFF).await;
 
         Timer::after(Duration::from_micros(1000)).await;
-
     }
-}
-
-async fn write_cycle<'d, P: Instance, const SC: usize, const SW: usize>(
-    control_sm: &mut StateMachine<'d, P, SC>,
-    write_sm: &mut StateMachine<'d, P, SW>,
-    addr: u8, data: u8
-) {
-    let data_payload = 0xFF00_0000u32 | ((data as u32) << 16);
-    let addr_payload = 0x0000_0000u32 | ((addr as u32) << 24);
-    write_sm.tx().wait_push(data_payload).await;
-    control_sm.tx().wait_push(addr_payload).await;
-}
-
-async fn read_cycle<'d, P: Instance, const SC: usize, const SR: usize>(
-    control_sm: &mut StateMachine<'d, P, SC>,
-    read_sm: &mut StateMachine<'d, P, SR>,
-    addr: u8
-) -> u8 {
-    let addr_payload = 0x0001_0000u32 | ((addr as u32) << 24);
-    control_sm.tx().wait_push(addr_payload).await;
-    read_sm.rx().wait_pull().await as u8
 }
---
+++ rp2040_fred/firmware/src/transport/transport_master/bus.rs
@@ -0,0 +1,226 @@
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::PIO0;
+use embassy_rp::pio::{
+    Common, Config, Direction, Instance, InterruptHandler, Pio, PioBatch, ShiftConfig,
+    ShiftDirection, StateMachine,
+};
+use embassy_rp::pio_programs::clock_divider::calculate_pio_clock_divider_value;
+use rp_pac as pac;
+
+use crate::resources::PioResources;
+
+macro_rules! log_info {
+    ($($arg:tt)*) => {
+        defmt::info!($($arg)*);
+    };
+}
+
+bind_interrupts!(struct Pio0Irqs {
+    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+// bind_interrupts!(struct Pio1Irqs {
+//     PIO1_IRQ_0 => InterruptHandler<PIO1>;
+// });
+
+pub struct Bus<
+    'sm,
+    PIO: Instance,
+    const SCL: usize,
+    const SC: usize,
+    const SW: usize,
+    const SR: usize,
+> {
+    _common: Common<'sm, PIO>,
+    _dir_a: Output<'sm>,
+    _clock: StateMachine<'sm, PIO, SCL>,
+    control: StateMachine<'sm, PIO, SC>,
+    write: StateMachine<'sm, PIO, SW>,
+    read: StateMachine<'sm, PIO, SR>,
+}
+
+// with concrete SM assignments
+pub type ThisBus<'a> = Bus<'a, PIO0, 0, 1, 2, 3>;
+
+impl<'a> ThisBus<'a> {
+    pub async fn write_cycle(&mut self, addr: u8, data: u8) {
+        let data_payload = 0xFF00_0000u32 | ((data as u32) << 16);
+        // let addr_payload = 0x0000_0000u32 | ((addr as u32) << 24);
+        let addr_payload = (addr as u32) << 24;
+        self.write.tx().wait_push(data_payload).await;
+        self.control.tx().wait_push(addr_payload).await;
+    }
+
+    pub async fn read_cycle(&mut self, addr: u8) -> u8 {
+        let addr_payload = 0x0001_0000u32 | ((addr as u32) << 24);
+        self.control.tx().wait_push(addr_payload).await;
+        self.read.rx().wait_pull().await as u8
+        // self.read.rx().pull() as u8
+    }
+
+    pub fn setup(pio_resources: PioResources) -> ThisBus<'a> {
+        // address data-dir high for output.
+        let mut dir_a = Output::new(pio_resources.pin_26, Level::High);
+        dir_a.set_high();
+
+        let fred_bm_clock = pio::pio_file!(
+            "../pio/bus_master.pio",
+            select_program("fred_bm_clock"),
+            options(max_program_size = 32)
+        );
+
+        let fred_bm_control = pio::pio_file!(
+            "../pio/bus_master.pio",
+            select_program("fred_bm_control"),
+            options(max_program_size = 32)
+        );
+
+        let fred_bm_data_write = pio::pio_file!(
+            "../pio/bus_master.pio",
+            select_program("fred_bm_data_write"),
+            options(max_program_size = 32)
+        );
+
+        let fred_bm_data_read = pio::pio_file!(
+            "../pio/bus_master.pio",
+            select_program("fred_bm_data_read"),
+            options(max_program_size = 32)
+        );
+
+        let mut pio0 = Pio::new(pio_resources.pio0, Pio0Irqs);
+
+        let mut clock = pio0.sm0;
+        let mut control = pio0.sm1;
+        let mut write = pio0.sm2;
+        let mut read = pio0.sm3;
+
+        let clock_program = pio0.common.load_program(&fred_bm_clock.program);
+        let control_program = pio0.common.load_program(&fred_bm_control.program);
+        let write_program = pio0.common.load_program(&fred_bm_data_write.program);
+        let read_program = pio0.common.load_program(&fred_bm_data_read.program);
+
+        log_info!("PIO programs loaded");
+
+        let p0 = pio0.common.make_pio_pin(pio_resources.pin_0);
+        let p1 = pio0.common.make_pio_pin(pio_resources.pin_1);
+        let p2 = pio0.common.make_pio_pin(pio_resources.pin_2);
+        let p3 = pio0.common.make_pio_pin(pio_resources.pin_3);
+        let p4 = pio0.common.make_pio_pin(pio_resources.pin_4);
+        let p5 = pio0.common.make_pio_pin(pio_resources.pin_5);
+        let p6 = pio0.common.make_pio_pin(pio_resources.pin_6);
+        let p7 = pio0.common.make_pio_pin(pio_resources.pin_7);
+        let p8 = pio0.common.make_pio_pin(pio_resources.pin_8);
+        let p9 = pio0.common.make_pio_pin(pio_resources.pin_9);
+        let p10 = pio0.common.make_pio_pin(pio_resources.pin_10);
+        let p11 = pio0.common.make_pio_pin(pio_resources.pin_11);
+        let p12 = pio0.common.make_pio_pin(pio_resources.pin_12);
+        let p13 = pio0.common.make_pio_pin(pio_resources.pin_13);
+        let p14 = pio0.common.make_pio_pin(pio_resources.pin_14);
+        let p15 = pio0.common.make_pio_pin(pio_resources.pin_15);
+        let p16 = pio0.common.make_pio_pin(pio_resources.pin_16);
+        let p17 = pio0.common.make_pio_pin(pio_resources.pin_17);
+        let p18 = pio0.common.make_pio_pin(pio_resources.pin_18);
+        let p19 = pio0.common.make_pio_pin(pio_resources.pin_19);
+        let p20 = pio0.common.make_pio_pin(pio_resources.pin_20);
+        let p27 = pio0.common.make_pio_pin(pio_resources.pin_27);
+
+        let data_bus_pins = [&p0, &p1, &p2, &p3, &p4, &p5, &p6, &p7];
+
+        let addr_bus_pins = [&p8, &p9, &p10, &p11, &p12, &p13, &p14, &p15];
+
+        let control_pins = [
+            &p17, // RnW
+            &p18, // NMI
+            &p19, // IRQ
+            &p20, // FRED
+        ];
+
+        let clock_pins = [
+            &p16, // 1MHzE
+        ];
+
+        let data_dir_pin = [&p27];
+
+        clock.set_pin_dirs(Direction::In, &data_bus_pins);
+        clock.set_pin_dirs(Direction::Out, &addr_bus_pins);
+        clock.set_pin_dirs(Direction::Out, &data_dir_pin);
+
+        // setting up control pins for open-drain output
+        clock.set_pins(Level::Low, &control_pins);
+        clock.set_pin_dirs(Direction::Out, &control_pins);
+        clock.set_pins(Level::Low, &clock_pins);
+        clock.set_pin_dirs(Direction::Out, &clock_pins);
+
+        clock.clear_fifos();
+        control.clear_fifos();
+        write.clear_fifos();
+        read.clear_fifos();
+
+        let mut clock_cfg = Config::default();
+        clock_cfg.use_program(&clock_program, &clock_pins);
+        clock_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 16_000_000);
+        clock.set_config(&clock_cfg);
+
+        let mut control_cfg = Config::default();
+        control_cfg.use_program(&control_program, &control_pins);
+        control_cfg.set_out_pins(&addr_bus_pins);
+        control_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 40_000_000);
+        control_cfg.shift_out = ShiftConfig {
+            threshold: 32,
+            direction: ShiftDirection::Left,
+            auto_fill: false,
+        };
+        control.set_config(&control_cfg);
+
+        let mut write_cfg = Config::default();
+        write_cfg.use_program(&write_program, &data_dir_pin);
+        write_cfg.set_out_pins(&data_bus_pins);
+        write_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 20_000_000);
+        write_cfg.shift_out = ShiftConfig {
+            threshold: 8,
+            direction: ShiftDirection::Left,
+            auto_fill: false,
+        };
+        write.set_config(&write_cfg);
+
+        let mut read_cfg = Config::default();
+        read_cfg.use_program(&read_program, &[]);
+        read_cfg.set_in_pins(&data_bus_pins);
+        read_cfg.clock_divider = calculate_pio_clock_divider_value(125_000_000, 50_000_000);
+        read_cfg.shift_in = ShiftConfig {
+            threshold: 8,
+            direction: ShiftDirection::Left,
+            auto_fill: false,
+        };
+        read.set_config(&read_cfg);
+
+        let mut batch = PioBatch::new();
+        batch.restart(&mut clock);
+        batch.set_enable(&mut clock, true);
+        batch.restart(&mut control);
+        batch.set_enable(&mut control, true);
+        batch.restart(&mut write);
+        batch.set_enable(&mut write, true);
+        batch.restart(&mut read);
+        batch.set_enable(&mut read, true);
+        batch.execute();
+
+        [16, 17, 18, 19, 20].iter().for_each(|n| {
+            pac::PADS_BANK0.gpio(*n).modify(|w| {
+                w.set_pue(true); // pull-up enable
+                w.set_pde(false); // pull-down disable
+            });
+        });
+
+        log_info!("PIO initialised on core1");
+
+        ThisBus {
+            _clock: clock,
+            control,
+            write,
+            read,
+            _dir_a: dir_a,
+            _common: pio0.common,
+        }
+    }
+}
