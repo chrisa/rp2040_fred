620d246d7f1d399a3073f51a10a478fba5c99a97
Chris Andrews <chris@nodnol.org>
2026-05-31 15:29:59 +0100

    Tidy / ignoring in repo.

---
+++ rp2040_fred/copy.sh
@@ -0,0 +1,3 @@
+(cd firmware && rsync -vr src pi@192.168.3.87:rp2040_fred/rp2040_fred/firmware/)
+(cd host && rsync -vr src pi@192.168.3.87:rp2040_fred/rp2040_fred/host/)
+(cd protocol && rsync -vr src pi@192.168.3.87:rp2040_fred/rp2040_fred/protocol/)
